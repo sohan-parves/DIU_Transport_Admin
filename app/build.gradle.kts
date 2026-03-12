88899a7bcdc6dc25d88c8370eb42c474a0e1635e
sohanparves <rafsanahamedsohan@gmail.com>
2026-03-12 10:10:54 +0600

    1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.sohan.diutransportadmin"
         minSdk = 30
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
